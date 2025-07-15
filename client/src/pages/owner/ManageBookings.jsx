41037da2c23232fb207e8ab8b8fc42033086681f
ThanhTung1909 <phamthanhtung200698@gmail.com>
2025-07-15 11:07:13 +0700

    implement manage bookings

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { assets, dummyMyBookingsData } from "../../assets/assets";
+import Title from "../../components/owner/Title";
 
 const ManageBookings = () => {
-  return (
-    <div>ManageBookings</div>
-  )
-}
+  const [bookings, setbookings] = useState([]);
+  const currency = import.meta.env.VITE_CURRENCY;
 
-export default ManageBookings
+  const fetchOwnerbookings = async () => {
+    setbookings(dummyMyBookingsData);
+  };
+
+  useEffect(() => {
+    fetchOwnerbookings();
+  }, []);
+  return (
+    <div className="px-4 pt-10 md:px-10 w-full ">
+      <Title
+        title="Manage Bookings"
+        subTitle="Track all customer bookings, approve or cancel requests, and manage booking statuses"
+      />
+
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead className="text-gray-500">
+            <tr>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium max-md:hidden">Date Range</th>
+              <th className="p-3 font-medium">Total</th>
+              <th className="p-3 font-medium max-md:hidden">Payment</th>
+              <th className="p-3 font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking, index) => (
+              <tr
+                key={index}
+                className="border-t border-borderColor text-gray-500"
+              >
+                <td className="p-3 flex items-center gap-3">
+                  <img
+                    src={booking.car.image}
+                    alt=""
+                    className="h-12 w-12 aspect-square rounded-md object-cover"
+                  />
+                  <div className="max-md:hidden">
+                    <p className="font-medium">
+                      {booking.car.brand} {booking.car.model}
+                    </p>
+                  </div>
+                </td>
+                <td className="p-3 max-md:hidden">
+                  {booking.pickupDate.split("T")[0]}
+                  <span> to </span>
+                  {booking.returnDate.split("T")[0]}
+                </td>
+
+                <td className="p-3">
+                  {currency}
+                  {booking.price}
+                </td>
+                <td className="p-3 max-md:hidden">
+                  <span className="bg-gray-100 px-3 py-1 rounded-full text-xs">offline</span>
+                </td>
+                <td className="p-3">
+                  {booking.status === 'pending' ? (
+                    <select value={booking.status} className="px-2 py-1.5 mt-1 text-gray-500
+                    border border-borderColor rounded-md outline-none">
+                      <option value="pending">Pending</option>
+                      <option value="cancelled">Cancelled</option>
+                      <option value="confirmed">Confirmed</option>
+                    </select>
+                  ) : (
+                    <span
+                    className={`px-3 py-1 rounded-full text-xs font-semibold ${
+                      booking.status === "confirmed"
+                        ? "bg-green-100 text-green-500"
+                        : booking.status === "pending"
+                        ? "bg-yellow-100 text-yellow-500"
+                        : "bg-blue-100 text-blue-500"
+                    }`}
+                  >
+                    {booking.status}
+                  </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ManageBookings;
